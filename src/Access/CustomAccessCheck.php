99dd000143e5c667c37f43813c6ac9fbc59a57ba
naresh-qed42 <naresh.bavaskar@qed42.com>
2019-11-13 20:21:42 +0530

    Menu links items activity added.

---
+++ src/Access/CustomAccessCheck.php
@@ -0,0 +1,28 @@
+<?php
+namespace Drupal\activities_d8\Access;
+
+use Drupal\Core\Routing\Access\AccessInterface;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\Core\Access\AccessResult;
+
+/**
+ * Checks access for displaying configuration translation page.
+ */
+class CustomAccessCheck implements AccessInterface {
+
+  /**
+   * A custom access check.
+   *
+   * @param \Drupal\Core\Session\AccountInterface $account
+   *   Run access checks for this account.
+   *
+   * @return \Drupal\Core\Access\AccessResultInterface
+   *   The access result.
+   */
+  public function access(AccountInterface $account) {
+    // Check permissions and combine that with any custom access checking needed. Pass forward
+    // parameters from the route and/or request as needed.
+    return  ($account->hasPermission('Access training content')) ? AccessResult::allowed() : AccessResult::forbidden();
+  }
+
+}
