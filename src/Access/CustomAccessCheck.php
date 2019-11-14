a55e1392df76787e2a52af6ef4cfa3211dd62a91
naresh-qed42 <naresh.bavaskar@qed42.com>
2019-11-14 13:25:41 +0530

    Persmission activities added.

--- src/Access/CustomAccessCheck.php
+++ src/Access/CustomAccessCheck.php
@@ -4,6 +4,7 @@ namespace Drupal\activities_d8\Access;
 use Drupal\Core\Routing\Access\AccessInterface;
 use Drupal\Core\Session\AccountInterface;
 use Drupal\Core\Access\AccessResult;
+use Drupal\node\NodeInterface;
 
 /**
  * Checks access for displaying configuration translation page.
@@ -19,7 +20,8 @@ class CustomAccessCheck implements AccessInterface {
    * @return \Drupal\Core\Access\AccessResultInterface
    *   The access result.
    */
-  public function access(AccountInterface $account) {
+  public function access(AccountInterface $account, nodeInterface $nid) {
+    $account->id();exit;
     // Check permissions and combine that with any custom access checking needed. Pass forward
     // parameters from the route and/or request as needed.
     return  ($account->hasPermission('Access training content')) ? AccessResult::allowed() : AccessResult::forbidden();
