4f156c9d8aea7acc8d12755472e3f1cd4f182f0c
naresh-qed42 <naresh.bavaskar@qed42.com>
2019-11-20 15:04:08 +0530

    Custom event activity code added.

--- src/EventSubscriber/ActivitiesSubscriber.php
+++ src/EventSubscriber/ActivitiesSubscriber.php
@@ -5,6 +5,7 @@ namespace Drupal\activities_d8\EventSubscriber;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
+use Drupal\activities_d8\Event\NodeInsertDemoEvent;
 
 /**
  *
@@ -17,7 +18,21 @@ class ActivitiesSubscriber implements EventSubscriberInterface {
   public static function getSubscribedEvents() {
     return [
       KernelEvents::RESPONSE => 'onResponse',
+      NodeInsertDemoEvent::DEMO_NODE_INSERT => 'onDemoNodeInsert',
     ];
+  }
+
+  /**
+   *
+   */
+  public function onDemoNodeInsert(NodeInsertDemoEvent $event) {
+    $entity = $event->getEntity();
+    \Drupal::logger('event_subscriber_demo')->notice('New @type: @title. Created by: @owner',
+      [
+        '@type' => $entity->getType(),
+        '@title' => $entity->label(),
+        '@owner' => $entity->getOwner()->getDisplayName(),
+      ]);
   }
 
   /**
